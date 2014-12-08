79678da058124374064a60abf40bc0374bb2d525
Robert Redl <robert.redl@easytouch.cc>
2014-12-08 15:46:28 -0500

    new version 0.2.2
    case insensitive search, updated readme

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'robertredl:tabular',
   summary: 'Datatables for large or small datasets in Meteor',
-  version: '0.2.1',
+  version: '0.2.2',
   git: 'https://github.com/robertredl/meteor-tabular.git'
 });
 
