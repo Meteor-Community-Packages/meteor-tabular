33c70dd6e25db6f8ea576c3909ab97abc20720f6
Nicolas Lopez <nicolaslopezj@me.com>
2014-12-27 04:53:06 -0300

    Fix bug on autorun
    On line 88, in the ajax function, when the tracker makes the autorun the variable ```tabularTable``` doesn't take the new value.
    I was having problems when changing the table in the same template.

--- client/tabular.js
+++ client/tabular.js
@@ -8,13 +8,14 @@ Template.tabular.helpers({
 
 Template.tabular.rendered = function () {
   var template = this, table, $tableElement = template.$('table');
+  var tabularTable = null;
 
   // We put this all in a reactive computation so that the component args,
   // such as the selector, can reactively change and the table will adjust.
   template.autorun(function () {
     var collection, pub, fields, selector, columns, options, searchFields = [];
     var data = Template.currentData();
-    var tabularTable = data && data.table;
+    tabularTable = data && data.table;
 
     if (!(tabularTable instanceof Tabular.Table)) {
         throw new Error("You must pass Tabular.Table instance as the table attribute");
