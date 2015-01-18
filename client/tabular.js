0f36a818e714791b68373fc7c410001760ac0606
Eric Dobbertin <eric@dairystatedesigns.com>
2015-01-17 21:47:53 -0600

    make search work for numbers; closes #27

--- client/tabular.js
+++ client/tabular.js
@@ -220,14 +220,27 @@ Template.tabular.rendered = function () {
       return;
     }
 
-    var searches = _.map(searchFields, function(field) {
-      var m = {};
-      m[field] = {$regex: searchString};
+    // See if we can resolve the search string to a number,
+    // in which case we use an extra query because $regex
+    // matches string fields only.
+    var numSearchString = Number(searchString), searches = [];
+
+    _.each(searchFields, function(field) {
+      var m1 = {}, m2 = {};
+
+      // String search
+      m1[field] = {$regex: searchString};
       // DataTables searches are case insensitive by default
       if (searchCaseInsensitive !== false) {
-        m[field].$options = "i";
+        m1[field].$options = "i";
       }
-      return m;
+      searches.push(m1);
+
+      // Number search
+      if (!isNaN(numSearchString)) {
+        m2[field] = numSearchString;//{$where: '/' + numSearchString + '/.test(this.' + field + ')'};
+        searches.push(m2);
+      }
     });
 
     template.tabular.pubSelector.set(_.extend({}, selector, {$or: searches}));
