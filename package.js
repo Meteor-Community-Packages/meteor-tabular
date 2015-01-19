34aaa958061c3bf40c81a4ccfb5972a7b96ca66d
Eric Dobbertin <eric@dairystatedesigns.com>
2015-01-19 07:36:35 -0600

    bump version

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: 'aldeed:tabular',
   summary: 'Datatables for large or small datasets in Meteor',
-  version: '1.0.1',
+  version: '1.0.3',
   git: 'https://github.com/aldeed/meteor-tabular.git'
 });
 
