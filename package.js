3ecd259a3843b4eb479ef1b8dc9ebb1cbae53f49
Eric Dobbertin <eric@dairystatedesigns.com>
2015-09-28 11:31:36 -0500

    Meteor 1.2 fix

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: 'aldeed:tabular',
   summary: 'Datatables for large or small datasets in Meteor',
-  version: '1.4.0',
+  version: '1.4.1',
   git: 'https://github.com/aldeed/meteor-tabular.git'
 });
 
@@ -13,7 +13,15 @@ Npm.depends({
 
 Package.onUse(function(api) {
   api.versionsFrom(['METEOR@0.9.4', 'METEOR@1.0']);
-  api.use(['check', 'underscore', 'mongo', 'blaze', 'templating', 'reactive-var', 'tracker']);
+  api.use([
+    'check',
+    'underscore',
+    'mongo',
+    'blaze',
+    'templating',
+    'reactive-var',
+    'tracker'
+  ]);
 
   // jquery is a weak reference in case you want to use a different package or
   // pull it in another way, but regardless you need to make sure it is loaded
@@ -35,12 +43,25 @@ Package.onUse(function(api) {
     'client/tableRecords.js',
     'client/tableInit.js',
     'client/pubSelector.js',
-    'client/tabular.js',
-    // images
-    'images/sort_asc.png',
-    'images/sort_asc_disabled.png',
-    'images/sort_both.png',
-    'images/sort_desc.png',
-    'images/sort_desc_disabled.png'
+    'client/tabular.js'
   ], 'client');
+
+  // images
+  if (typeof api.addAssets === 'function') {
+    api.addAssets([
+      'images/sort_asc.png',
+      'images/sort_asc_disabled.png',
+      'images/sort_both.png',
+      'images/sort_desc.png',
+      'images/sort_desc_disabled.png'
+    ], 'client');
+  } else {
+    api.addFiles([
+      'images/sort_asc.png',
+      'images/sort_asc_disabled.png',
+      'images/sort_both.png',
+      'images/sort_desc.png',
+      'images/sort_desc_disabled.png'
+    ], 'client');
+  }
 });
