03ebfee7cf538d430ff51a701beb5297815c5036
Lucendio <github+dev@lucendio.com>
2016-05-02 15:52:57 +0200

    added session as explicit dependency
    because it breaks when this package is used by a project that does not depend on `Session`

--- package.js
+++ package.js
@@ -22,6 +22,10 @@ Package.onUse(function(api) {
     'reactive-var',
     'tracker'
   ]);
+  
+  api.use([
+    'session'  
+  ], 'client');
 
   // jquery is a weak reference in case you want to use a different package or
   // pull it in another way, but regardless you need to make sure it is loaded
@@ -47,21 +51,15 @@ Package.onUse(function(api) {
   ], 'client');
 
   // images
-  if (typeof api.addAssets === 'function') {
-    api.addAssets([
-      'images/sort_asc.png',
-      'images/sort_asc_disabled.png',
-      'images/sort_both.png',
-      'images/sort_desc.png',
-      'images/sort_desc_disabled.png'
-    ], 'client');
-  } else {
-    api.addFiles([
-      'images/sort_asc.png',
-      'images/sort_asc_disabled.png',
-      'images/sort_both.png',
-      'images/sort_desc.png',
-      'images/sort_desc_disabled.png'
-    ], 'client');
+  var addImages = api.addAssets;
+  if (typeof addImages !== 'function') {
+    addImages = api.addFiles;
   }
+  addImages([
+    'images/sort_asc.png',
+    'images/sort_asc_disabled.png',
+    'images/sort_both.png',
+    'images/sort_desc.png',
+    'images/sort_desc_disabled.png'
+  ], 'client');
 });
