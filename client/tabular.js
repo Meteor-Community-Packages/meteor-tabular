20c5c335621c2e98f3158aca8ae6360ca163b5f7
Leonid Gaiazov <gaiazov@gmail.com>
2016-05-20 17:17:34 -0400

    Fixed an issue where processing indicator wouldn't go away Fixed an issue where the processing indicator wouldn't go away. This happened when the pubSelector was changed, but the change didn't result in any new rows.
    For example, if you have a collection with 30 rows, going from page size 50 to page size 100 doesn't actually get any new rows. So some of the autorun blocks were never triggered.

--- client/tabular.js
+++ client/tabular.js
@@ -208,7 +208,28 @@ var tabularOnRendered = function () {
       template.tabular.pubSelector.get(),
       template.tabular.sort.get(),
       template.tabular.skip.get(),
-      template.tabular.limit.get()
+      template.tabular.limit.get(),
+        function() {
+          // this callback is there only to handle a case where a change in pubSelector
+          // doesn't actually get any new rows. This causes the 'processing' indicator 
+          // to be displayed but never hidden
+
+          // most of this code is copy & pasted from other autorun blocks
+          var tableName = Tracker.nonreactive(function () {
+            return template.tabular.tableName.get();
+          });
+
+          var tableInfo = Tabular.getRecord(tableName) || {};
+
+          var collection = template.tabular.collection.get();
+
+          // we only care about the count, so no fields selector
+          // if cursor already has all the records, hide the processing indicator
+          var cursorCount = collection.find({_id: {$in: tableInfo.ids}}).count();
+          if (cursorCount === tableInfo.ids.length) {
+            template.tabular.isLoading.set(false);
+          }
+        }
     );
   });
 
