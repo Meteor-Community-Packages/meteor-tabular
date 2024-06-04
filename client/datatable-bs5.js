6319818a1229a377ec137360e9c521baec02073e
Pierre Wieser <p.wieser@trychlos.org>
2024-06-04 22:15:05 +0200

    Integrate with Datatable 2.0.8

---
+++ client/datatable-bs5.js
@@ -0,0 +1,19 @@
+
+// pwi 2024- 6- 4
+//  try here to mutualize the datatable imports
+//  begin
+import DataTable from 'datatables.net-bs5';
+import 'datatables.net-buttons-bs5';
+import 'datatables.net-buttons/js/buttons.colVis.mjs';
+import 'datatables.net-buttons/js/buttons.print.mjs';
+import 'datatables.net-colreorder-bs5';
+import 'datatables.net-fixedheader-bs5';
+import 'datatables.net-responsive-bs5';
+import 'datatables.net-scroller-bs5';
+//
+import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
+import 'datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css';
+import 'datatables.net-colreorder-bs5/css/colReorder.bootstrap5.min.css';
+import 'datatables.net-fixedheader-bs5/css/fixedHeader.bootstrap5.min.css';
+import 'datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css';
+import 'datatables.net-scroller-bs5/css/scroller.bootstrap5.min.css';
