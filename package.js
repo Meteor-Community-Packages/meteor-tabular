7f8a84e59e134a32e4104a6653d09d1bbc181301
jankapunkt <jkuester@uni-bremen.de>
2024-09-04 21:38:39 +0200

    publish 3.0.0-rc.2

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: 'aldeed:tabular',
   summary: 'Datatables for large or small datasets in Meteor',
-  version: '3.0.0-rc.1',
+  version: '3.0.0-rc.2',
   git: 'https://github.com/Meteor-Community-Packages/meteor-tabular.git'
 });
 
